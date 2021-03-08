e100212ac1b504fead6587fc157d5ce50f0dcc16
Jhovanny <Jhovanny.linares@gmail.com>
2021-03-08 16:21:35 -0600

    config Segurity

--- CotsWS/src/main/java/mx/morena/TestApplication.java
+++ CotsWS/src/main/java/mx/morena/TestApplication.java
@@ -25,10 +25,12 @@ public class TestApplication {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http.csrf().disable()
-					.addFilterAfter(new JWTAuthorizationFilter(),
-							UsernamePasswordAuthenticationFilter.class)
-					.authorizeRequests().antMatchers(HttpMethod.POST, "/user").permitAll().anyRequest().authenticated();
-		}
+			.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+			.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/**").permitAll()
+			.antMatchers(HttpMethod.GET, "/cots/**").authenticated()
+			.anyRequest()
+			.authenticated();
 	}
 
 }
