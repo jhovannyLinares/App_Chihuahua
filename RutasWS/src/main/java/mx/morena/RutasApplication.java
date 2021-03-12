f7fe8292e745d524f987e2fe886d65a47a014b5f
luis jesus mancilla segura <luis_mase@outlook.com>
2021-03-12 09:47:11 -0600

    se agrega rutasWS

---
+++ RutasWS/src/main/java/mx/morena/RutasApplication.java
@@ -0,0 +1,13 @@
+package mx.morena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvencidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvencidosApplication.class, args);
+	}
+
+}
---
+++ RutasWS/src/main/java/mx/morena/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package mx.morena;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import mx.morena.security.servicio.JWTAuthorizationFilter;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors()
+				.and().csrf().disable()
+				.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/**").permitAll()
+				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+				.antMatchers("/convencidos/**")
+				.authenticated().anyRequest().authenticated()
+				.and().addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+}
