25a25d454583c5cd0720616a14b5bce0a9321013
Ismael <ismaeldom11@gmail.com>
2021-04-12 13:33:45 -0500

    dto

---
+++ CasillasWS/src/main/java/mx/morena/negocio/dto/ReporteCrgDTO.java
@@ -0,0 +1,106 @@
+package mx.morena.negocio.dto;
+
+public class ReporteCrgDTO {
+	private Long id;
+	
+	//private String metaRg;
+	
+	private Long metaRg;
+	
+	private Long avanceCapturadoRg;
+	
+	private Long avanceAsignadoRg;
+	
+	private Double porcentajeAvanceRg;
+	
+	private Long metaRc;
+	
+	private Long avanceCapturadoRc;
+	
+	private Long avanceAsignadoRc;
+	
+	private Long avance;
+	
+	private Double porcentajeAvanceRc;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getMetaRg() {
+		return metaRg;
+	}
+
+	public void setMetaRg(Long metaRg) {
+		this.metaRg = metaRg;
+	}
+
+	public Long getAvanceCapturadoRg() {
+		return avanceCapturadoRg;
+	}
+
+	public void setAvanceCapturadoRg(Long avanceCapturadoRg) {
+		this.avanceCapturadoRg = avanceCapturadoRg;
+	}
+
+	public Long getAvanceAsignadoRg() {
+		return avanceAsignadoRg;
+	}
+
+	public void setAvanceAsignadoRg(Long avanceAsignadoRg) {
+		this.avanceAsignadoRg = avanceAsignadoRg;
+	}
+
+	public Double getPorcentajeAvanceRg() {
+		return porcentajeAvanceRg;
+	}
+
+	public void setPorcentajeAvanceRg(Double porcentajeAvanceRg) {
+		this.porcentajeAvanceRg = porcentajeAvanceRg;
+	}
+
+	public Long getMetaRc() {
+		return metaRc;
+	}
+
+	public void setMetaRc(Long metaRc) {
+		this.metaRc = metaRc;
+	}
+
+	public Long getAvanceCapturadoRc() {
+		return avanceCapturadoRc;
+	}
+
+	public void setAvanceCapturadoRc(Long avanceCapturadoRc) {
+		this.avanceCapturadoRc = avanceCapturadoRc;
+	}
+
+	public Long getAvanceAsignadoRc() {
+		return avanceAsignadoRc;
+	}
+
+	public void setAvanceAsignadoRc(Long avanceAsignadoRc) {
+		this.avanceAsignadoRc = avanceAsignadoRc;
+	}
+
+	public Long getAvance() {
+		return avance;
+	}
+
+	public void setAvance(Long avance) {
+		this.avance = avance;
+	}
+
+	public Double getPorcentajeAvanceRc() {
+		return porcentajeAvanceRc;
+	}
+
+	public void setPorcentajeAvanceRc(Double porcentajeAvanceRc) {
+		this.porcentajeAvanceRc = porcentajeAvanceRc;
+	}
+	
+}
