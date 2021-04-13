5978cf7d18addb25b7a3d4d816d4a6f669d8e060
Ismael <ismaeldom11@gmail.com>
2021-04-13 10:03:08 -0500

    RowMapper

---
+++ Commons/src/main/java/mx/morena/persistencia/rowmapper/LongRowMapper.java
@@ -0,0 +1,15 @@
+package mx.morena.persistencia.rowmapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class LongRowMapper implements RowMapper<Long> {
+
+	@Override
+	public Long mapRow(ResultSet rs, int rowNum) throws SQLException {
+		return rs.getLong(1);
+	}
+
+}
