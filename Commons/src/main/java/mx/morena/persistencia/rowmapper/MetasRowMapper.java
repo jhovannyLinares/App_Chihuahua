edd8d9bbc27a4fdb365c06ffdac5839eaf85dbd4
luis jesus mancilla segura <luis_mase@outlook.com>
2021-05-12 16:22:53 -0500

    row mapper metas

--- Commons/src/main/java/mx/morena/persistencia/rowmapper/MetasRowMapper.java
+++ Commons/src/main/java/mx/morena/persistencia/rowmapper/MetasRowMapper.java
@@ -2,6 +2,7 @@ package mx.morena.persistencia.rowmapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.jdbc.core.RowMapper;
@@ -12,8 +13,24 @@ public class MetasRowMapper implements RowMapper<List<Metas>> {
 
 	@Override
 	public List<Metas> mapRow(ResultSet rs, int rowNum) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Metas> lstMetas = new ArrayList<Metas>();
+		Metas metas = null;
+		
+		do {
+			metas = new Metas();
+			
+			metas.setListaNominal(rs.getLong("listado_nominal"));
+			metas.setMetaCrg(rs.getLong("meta_crg"));
+			metas.setMetaRg(rs.getLong("meta_rg"));
+			metas.setMetaRc(rs.getLong("meta_rc"));
+			metas.setMetaConvencidos(rs.getLong("meta_convencidos"));
+			
+			lstMetas.add(metas);
+			
+		}while (rs.next());
+		
+		return lstMetas;
 	}
 
 }
