8d4bdabb949699f93620ee42dd0532cae32bd72b
Ismael <ismaeldom11@gmail.com>
2021-05-17 12:17:18 -0500

    Add catalogo de motivos termino de votacion

---
+++ Commons/src/main/java/mx/morena/persistencia/entidad/MotivosTerminoVotacion.java
@@ -0,0 +1,24 @@
+package mx.morena.persistencia.entidad;
+
+public class MotivosTerminoVotacion {
+	
+	private Long id;
+	private String motivo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMotivo() {
+		return motivo;
+	}
+
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+
+}
